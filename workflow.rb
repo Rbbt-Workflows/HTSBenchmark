6b5f86dd949f4ffe2483c905e7650476471d6879
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-08-14 14:39:38 +0200

    Added Benchmarking code for mutect2

--- workflow.rb
+++ workflow.rb
@@ -1,6 +1,5 @@
 require 'rbbt-util'
 require 'rbbt/workflow'
-
 Misc.add_libdir if __FILE__ == $0
 
 #require 'rbbt/sources/HTSBenchmark'
@@ -9,76 +8,109 @@ Workflow.require_workflow "HTS"
 module HTSBenchmark
   extend Workflow
 
-  CALLERS = %w(mutect2 strelka varscan_somatic muse somatic_sniper)
+   CALLERS = %w(mutect2 strelka varscan_somatic muse somatic_sniper)
 
-  dep HTS, :BAM, :fastq1 => :placeholder, :fastq2 => :placeholder do |jobname,options|
-    inputs = case jobname
-             when 'tumor'
-               {:fastq1 => Rbbt.data.gatk["tumor.1.fastq"].find, :fastq2 => Rbbt.data.gatk["tumor.2.fastq"].find}
-             when 'normal'
-               {:fastq1 => Rbbt.data.gatk["normal.1.fastq"].find, :fastq2 => Rbbt.data.gatk["normal.2.fastq"].find}
-             end
-    {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.GATK.' + jobname}
-  end
-  extension :bam
-  task :BAM => :binary do
-    Open.rm self.path
-    Open.ln_s step(:BAM_rescore).path, self.path
-    nil
-  end
+   dep HTS, :BAM, :fastq1 => :placeholder, :fastq2 => :placeholder do |jobname,options|
+     inputs = case jobname
+              when 'tumor'
+                {:fastq1 => Rbbt.data.gatk["tumor.1.fastq"].find, :fastq2 => Rbbt.data.gatk["tumor.2.fastq"].find}
+              when 'normal'
+                {:fastq1 => Rbbt.data.gatk["normal.1.fastq"].find, :fastq2 => Rbbt.data.gatk["normal.2.fastq"].find}
+              end
+     {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.GATK.' + jobname}
+   end
+   extension :bam
+   task :BAM => :binary do
+     Open.rm self.path
+     Open.ln_s step(:BAM_rescore).path, self.path
+     nil
+   end
 
-  CALLERS.each do |var_caller|
-    dep :BAM, :compute => :produce do |jobname, options|
-      %w(tumor normal).collect do |type|
-        {:inputs => options, :jobname => type}
-      end
-    end
-    extension :vcf
-    dep_task var_caller, HTS, var_caller, :tumor => :BAM, :normal => :BAM do |jobname,options,deps|
-        normal = deps.flatten.select{|d| d.task_name.to_s == 'BAM' && d.clean_name.include?('normal')}.first
-        tumor = deps.flatten.select{|d| d.task_name.to_s == 'BAM' && d.clean_name.include?('tumor')}.first
+   input :bamfile, :file, "BAM file", nil, :nofile => true
+   task :sortBAM => :binary do |bamfile|
+      args= {}
+      args["--INPUT"] = bamfile
+      args["--OUTPUT"] = self.tmp_path
+      args["--SORT_ORDER"] = 'coordinate'
+      args.merge(:pipe => true)
+      CMD.cmd("gatk SortSam", args)
+      Open.mv self.tmp_path, self.path
+   end
 
-        {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
-    end
-  end
+   input :bamfile, :file, "BAM file", nil, :nofile => true
+   input :newSM , :string, "New sample name", nil
+   task :editBAMSM => :binary do |bamfile,newSM|
+      args = {}
+      args.merge(:pipe => true)
+      line = CMD.cmd("samtools view -H #{bamfile} | grep '\@RG'", args).read.split
+      values = Hash[line.map{|i| [i.split(':')[0],i.split(':')[1] ] }]
 
-  dep :strelka, :compute => :bootstrap, :tumor => :placeholder, :normal => :placeholder do |jobname, options|
-    CALLERS.collect do |var_caller|
-      {:task => var_caller, :inputs => options, :jobname => jobname}
-    end
-  end
-  task :caller_comparison => :tsv do
-    caller_variants = {}
-    dependencies.each do |dep|
-      var_caller = dep.task_name
+      args = {}
+	  args["-I"] = bamfile
+      args["-O"] = self.tmp_path
+	  args["-ID"] = values["ID"]
+	  args["-LB"] = values["LB"]
+	  args["-PL"] = values["PL"]
+	  args["-PU"] = values["PU"]
+	  args["-SM"] = newSM
 
-      variants = TSV.traverse dep, :into => [] do |chr, values|
-        pos, id, ref, alt, *rest = values
+	  CMD.cmd("gatk AddOrReplaceReadGroups",args)
+      Open.mv self.tmp_path, self.path
+   end
 
-        [chr, pos, alt] * ":"
-      end
-      caller_variants[var_caller] = variants
-    end
+   dep :BAM, :jobname => 'normal'
+   extension :vcf
+   dep_task :haplotype, HTS, :haplotype, :BAM => :BAM
 
-    var_callers = caller_variants.keys.sort
-    tsv = TSV.setup({}, :key_field => "Caller", :fields => ["Unique"] + var_callers, :type => :list, :cast => :to_i)
-    var_callers.each do |c1|
-      m1 = caller_variants[c1]
-      matches = var_callers.collect do |c2|
-        next if c1 == c2
-        m2 = caller_variants[c2]
-        m1 & m2
-      end
-      unique = m1 - matches.flatten
-      tsv[c1] = [unique.length] + matches.collect{|m| m.nil? ? m1.length : m.length}
-    end
+   dep HTS, :revert_BAM do |jobname,options|
+      inputs = {:bam_file => Dir.glob(Rbbt.share.data.studies.BAMSURGEON_TEST_MUTECT["WES.orig"].bamsurgeon_test_mutect_normal["*.bam"].find)}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.' + jobname}
+   end
+   dep HTS,:BAM_rescore_realign
+   dep HTS, :BAMSurgeon_add_snvs do |jobname, options, deps|
+      bamfile = Samtools.prepare_BAM(deps.flatten.last.dependencies.last.path)
+      inputs = {:varfile => Rbbt.data.bam_surgeon["test_snvs.txt"].find,
+                :bamfile => bamfile,
+                :picardjar => Rbbt.software.opt.PicardTools["picard-2.20.5.jar"].find}
 
-    tsv
-  end
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.' + jobname}
+   end
+   extension :bam
+   task :test_muts => :binary do |jobname,options,deps|
+      Open.rm self.path
+      Open.ln_s step(:BAMSurgeon_add_snvs).path, self.path
+      nil
+   end
 
-  dep :BAM, :jobname => 'normal'
-  extension :vcf
-  dep_task :haplotype, HTS, :haplotype, :BAM => :BAM
+   dep :test_muts
+   dep :sortBAM do |jobname,options,deps|
+      bamfile = deps.flatten.select{|d| d.task_name.to_s == 'test_muts' }.first.path
+      inputs = {:bamfile => bamfile}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sorted_tumor' + jobname}
+   end
+   dep :editBAMSM do |jobname,options,deps|
+      bamfile = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.sorted_tumorbamsurgeon_mutect2" }.first.path
+      newSM = "synthetic_tumor"
+      inputs = {:bamfile => bamfile, :newSM => newSM}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.editedBAMSM' + jobname}
+   end
+   dep :sortBAM do |jobname,options,deps|
+      bamfile = deps.first.dependencies.flatten.select{|d| d.task_name.to_s == 'BAM_rescore_realign'}.first.path
+      inputs = {:bamfile => bamfile}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sorted_normal' + jobname}
+   end
+   dep HTS, :mutect2, :tumor => :placeholder, :normal => :placeholder do |jobname,options,deps|
+      tumor = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.editedBAMSMbamsurgeon_mutect2" }.first.path
+      normal = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.sorted_normalbamsurgeon_mutect2" }.first.path
+      {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
+   end
+   extension :vcf
+   task :mutect_benchmark => :binary do |jobname,options,deps|
+      Open.rm self.path
+      Open.ln_s step(:mutect2).path, self.path
+      nil
+   end
+
 end
 
 #require 'HTSBenchmark/tasks/basic.rb'
