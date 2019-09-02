e6d96ead5a6208c0006e2182c7653eaa66a54177
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-09-02 15:17:28 +0200

    fixes

--- workflow.rb
+++ workflow.rb
@@ -1,5 +1,6 @@
 require 'rbbt-util'
 require 'rbbt/workflow'
+
 Misc.add_libdir if __FILE__ == $0
 
 #require 'rbbt/sources/HTSBenchmark'
@@ -29,22 +30,22 @@ module HTSBenchmark
    input :bamfile, :file, "BAM file", nil, :nofile => true
    task :sortBAM => :binary do |bamfile|
       args= {}
+	
       args["--INPUT"] = bamfile
       args["--OUTPUT"] = self.tmp_path
       args["--SORT_ORDER"] = 'coordinate'
-      args.merge(:pipe => true)
+      #args.merge(:pipe => true)
       CMD.cmd("gatk SortSam", args)
       Open.mv self.tmp_path, self.path
    end
+
 
    input :bamfile, :file, "BAM file", nil, :nofile => true
    input :newSM , :string, "New sample name", nil
    task :editBAMSM => :binary do |bamfile,newSM|
       args = {}
-      args.merge(:pipe => true)
       line = CMD.cmd("samtools view -H #{bamfile} | grep '\@RG'", args).read.split
       values = Hash[line.map{|i| [i.split(':')[0],i.split(':')[1] ] }]
-
       args = {}
 	  args["-I"] = bamfile
       args["-O"] = self.tmp_path
@@ -53,9 +54,8 @@ module HTSBenchmark
 	  args["-PL"] = values["PL"]
 	  args["-PU"] = values["PU"]
 	  args["-SM"] = newSM
-
 	  CMD.cmd("gatk AddOrReplaceReadGroups",args)
-      Open.mv self.tmp_path, self.path
+ 	  Open.mv self.tmp_path, self.path
    end
 
    dep :BAM, :jobname => 'normal'
@@ -68,7 +68,7 @@ module HTSBenchmark
    end
    dep HTS,:BAM_rescore_realign
    dep HTS, :BAMSurgeon_add_snvs do |jobname, options, deps|
-      bamfile = Samtools.prepare_BAM(deps.flatten.last.dependencies.last.path)
+      bamfile = Samtools.prepare_BAM(deps.flatten.select{|d| d.task_name.to_s == 'BAM_rescore_realign' }.first.path)
       inputs = {:varfile => Rbbt.data.bam_surgeon["test_snvs.txt"].find,
                 :bamfile => bamfile,
                 :picardjar => Rbbt.software.opt.PicardTools["picard-2.20.5.jar"].find}
@@ -86,22 +86,31 @@ module HTSBenchmark
    dep :sortBAM do |jobname,options,deps|
       bamfile = deps.flatten.select{|d| d.task_name.to_s == 'test_muts' }.first.path
       inputs = {:bamfile => bamfile}
-      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sorted_tumor' + jobname}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sort_tumor.' + jobname}
    end
    dep :editBAMSM do |jobname,options,deps|
-      bamfile = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.sorted_tumorbamsurgeon_mutect2" }.first.path
+      bamfile = deps.flatten.select{|d| d.task_name.to_s == 'sortBAM' }.first.path 
       newSM = "synthetic_tumor"
       inputs = {:bamfile => bamfile, :newSM => newSM}
-      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.editedBAMSM' + jobname}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark' + jobname}
    end
    dep :sortBAM do |jobname,options,deps|
       bamfile = deps.first.dependencies.flatten.select{|d| d.task_name.to_s == 'BAM_rescore_realign'}.first.path
+      iii bamfile
       inputs = {:bamfile => bamfile}
-      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sorted_normal' + jobname}
+      {:inputs => options.merge(inputs), :jobname => 'HTSBenchmark.sort_normal.' + jobname}
    end
+   task :prepared_bams => :binary do |jobname,options,deps|
+      Open.rm self.path 
+      Open.ln_s step(:editBAMSM).path, self.path
+	  nil
+   end
+
+
+   dep :prepared_bams
    dep HTS, :mutect2, :tumor => :placeholder, :normal => :placeholder do |jobname,options,deps|
-      tumor = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.editedBAMSMbamsurgeon_mutect2" }.first.path
-      normal = deps.flatten.select{|d| d.clean_name == "HTSBenchmark.sorted_normalbamsurgeon_mutect2" }.first.path
+      tumor = deps.flatten.last.info[:dependencies].select{|d| d.include?:editBAMSM}.first.last
+      normal =deps.flatten.last.info[:dependencies].select{|d| d.include?:sortBAM}.select{|d| d.last.include?("sort_normal")}.first.last
       {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
    end
    extension :vcf
@@ -111,8 +120,47 @@ module HTSBenchmark
       nil
    end
 
-end
+   
+   dep :prepared_bams
+   dep HTS, :varscan_somatic, :tumor => :placeholder, :normal => :placeholder do |jobname,options,deps|
+      tumor = deps.flatten.last.info[:dependencies].select{|d| d.include?:editBAMSM}.first.last
+      normal =deps.flatten.last.info[:dependencies].select{|d| d.include?:sortBAM}.select{|d| d.last.include?("sort_normal")}.first.last
+      {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
+   end
+   extension :vcf
+   task :varscan_somatic_benchmark => :binary do |jobname,options,deps|
+      Open.rm self.path
+      Open.ln_s step(:varscan_somatic).path, self.path
+      nil
+   end
 
+   dep :prepared_bams
+   dep HTS, :somatic_sniper, :tumor => :placeholder, :normal => :placeholder do |jobname,options,deps|
+      tumor = deps.first.info[:dependencies].select {|a| a.include? :editBAMSM}.first[1]
+      normal = deps.first.info[:dependencies].last[1]
+      {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
+   end
+   extension :vcf
+   task :somatic_sniper_benchmark => :binary do |jobname,options,deps|
+      Open.rm self.path
+      Open.ln_s step(:somatic_sniper).path, self.path
+      nil
+   end
+
+   dep :prepared_bams
+   dep HTS, :muse, :tumor => :placeholder, :normal => :placeholder do |jobname,options,deps|
+      tumor = deps.flatten.last.info[:dependencies].select{|d| d.include?:editBAMSM}.first.last
+      normal =deps.flatten.last.info[:dependencies].select{|d| d.include?:sortBAM}.select{|d| d.last.include?("sort_normal")}.first.last
+      iii normal
+      {:inputs => options.merge({:normal => normal, :tumor => tumor}), :jobname => jobname}
+   end
+   extension :vcf
+   task :muse_benchmark => :binary do |jobname,options,deps|
+      Open.rm self.path
+      Open.ln_s step(:muse).path, self.path
+      nil
+   end
+end
 #require 'HTSBenchmark/tasks/basic.rb'
 
 #require 'rbbt/knowledge_base/HTSBenchmark'
